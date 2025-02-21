1c5f45edaf1172f5646996401f3910284b808559
Darlley <systembrtt@gmail.com>
2025-02-21 15:27:33 -0400

    redirect same user

--- src/functions/subscribe-to-event.ts
+++ src/functions/subscribe-to-event.ts
@@ -1,3 +1,4 @@
+import { eq } from 'drizzle-orm'
 import { db } from '../drizzle/client'
 import { subscriptions } from '../drizzle/schema/subscriptions'
 
@@ -10,6 +11,17 @@ export async function subscribeToEvent({
   name,
   email,
 }: SubvscribeToEventParams) {
+  const subscibers = await db
+    .select()
+    .from(subscriptions)
+    .where(eq(subscriptions.email, email))
+
+  if (subscibers.length > 0) {
+    return {
+      subscriberId: subscibers[0].id,
+    }
+  }
+
   const result = await db
     .insert(subscriptions)
     .values({
@@ -18,10 +30,8 @@ export async function subscribeToEvent({
     })
     .returning()
 
-  const { 
-    id: subscriberId 
-  } = result[0]
-  
+  const { id: subscriberId } = result[0]
+
   return {
     subscriberId,
   }
